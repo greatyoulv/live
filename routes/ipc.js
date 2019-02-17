58d9226ffd06fa4d936c0cc0007b95efcf1356e0
youlv <youlv@foxmail.com>
2019-02-17 14:31:44 +0800

    mongodb find

--- routes/ipc.js
+++ routes/ipc.js
@@ -2,39 +2,24 @@ var express = require('express');
 var router = express.Router();
 var process = require('child_process');
 
+var MongoClient = require('mongodb').MongoClient;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-var MongoClient = require('mongodb').MongoClient;
+  //var MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect('mongodb://localhost:27017/ipcs', function (err, client) {
-  if (err) throw err
-
-  var db = client.db('ipcs')
-  
-  db.collection('ipc').find({}).toArray(function (err, result) {
+  MongoClient.connect('mongodb://localhost:27017/ipcs', function (err, client) {
     if (err) throw err
 
-    console.log(result)
-    db.close()
-  });
-});
-  res.render('ipc', { title: 'Express' });
-});
-
-router.get('/ipc', function(req, res, next) {
-var MongoClient = require('mongodb').MongoClient;
-
-MongoClient.connect('mongodb://localhost:27017/ipcs', function (err, client) {
-  if (err) throw err
-
-  var db = client.db('ipcs')
+    var db = client.db('ipcs')
   
-  db.collection('ip').find().toArray(function (err, result) {
-    if (err) throw err
+    db.collection('ipc').find().toArray(function (err, result) {
+      if (err) throw err
 
-    console.log(result)
-  });
+      console.log(result)
+      res.render('ipc', { datas:result });
+    });
 });
+ // res.render('ipc', { datas: result });
 });
 
 module.exports = router;
