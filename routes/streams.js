7e2a6b12d6d58e6152ebaa173fe917266b1f6c51
youlv <youlv@foxmail.com>
2019-02-22 14:56:38 +0800

    edit UA

--- routes/streams.js
+++ routes/streams.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var ua = req.headers['user-agent'];
-  /Android|webOS|iPhone|iPod|BlackBerry/i.test(ua) ? res.redirect('/phonestreams') : res.redirect('/pcstreams') ;
+  /Android|webOS|iPhone|iPad|BlackBerry/i.test(ua) ? res.redirect('/phonestreams') : res.redirect('/pcstreams') ;
   //res.render('streams',{title: 'youlv' });
   
   //res.send(body.streams[1].app + ' ' + body.streams[1].name);
